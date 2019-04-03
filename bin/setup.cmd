6bd0b8a732fe506cc112f672349621db6f4e4b74
x86dev <info@x86dev.com>
2019-04-03 11:57:31 +0200

    Chocolatey package tweaks and bugfixes.

--- bin/setup.cmd
+++ bin/setup.cmd
@@ -1,16 +1,21 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH="%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 choco feature enable -n allowGlobalConfirmation
 
-set MY_CHOCO_INSTALL_CMD=choco --yes install
+set MY_CHOCO_INSTALL_CMD=choco install -v --yes 
 
 %MY_CHOCO_INSTALL_CMD% conemu
 %MY_CHOCO_INSTALL_CMD% curl
 %MY_CHOCO_INSTALL_CMD% firefox
 %MY_CHOCO_INSTALL_CMD% git
+%MY_CHOCO_INSTALL_CMD% gpg4win 
+%MY_CHOCO_INSTALL_CMD% greenshot 
 %MY_CHOCO_INSTALL_CMD% keepass
 %MY_CHOCO_INSTALL_CMD% putty
-%MY_CHOCO_INSTALL_CMD% rufus
+%MY_CHOCO_INSTALL_CMD% qownnotes
+%MY_CHOCO_INSTALL_CMD% livekd
+%MY_CHOCO_INSTALL_CMD% nextcloud-client 
+%MY_CHOCO_INSTALL_CMD% python2 
 %MY_CHOCO_INSTALL_CMD% spotify
 %MY_CHOCO_INSTALL_CMD% sumatrapdf
 %MY_CHOCO_INSTALL_CMD% sysinternals
@@ -20,13 +25,13 @@ set MY_CHOCO_INSTALL_CMD=choco --yes install
 %MY_CHOCO_INSTALL_CMD% tortoisesvn
 %MY_CHOCO_INSTALL_CMD% tortoisegit
 %MY_CHOCO_INSTALL_CMD% vim
+%MY_CHOCO_INSTALL_CMD% visualstudio2017community
 %MY_CHOCO_INSTALL_CMD% vlc
 %MY_CHOCO_INSTALL_CMD% vscode
+%MY_CHOCO_INSTALL_CMD% vscode-settingssync 
 %MY_CHOCO_INSTALL_CMD% wget
-%MY_CHOCO_INSTALL_CMD% livekd
-%MY_CHOCO_INSTALL_CMD% radare
-%MY_CHOCO_INSTALL_CMD% visualstudio2017community
 %MY_CHOCO_INSTALL_CMD% windbg
+%MY_CHOCO_INSTALL_CMD% xnviewmp
 
 bcdedit.exe -set loadoptions DDISABLE_INTEGRITY_CHECKS
 bcdedit.exe -set TESTSIGNING ON

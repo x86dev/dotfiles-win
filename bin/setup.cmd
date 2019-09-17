4e17488c1508382a58ad9c9814f766b146df5f69
x86dev <info@x86dev.com>
2019-09-17 17:50:39 +0200

    Updated bin/setup.cmd.

--- bin/setup.cmd
+++ bin/setup.cmd
@@ -2,21 +2,22 @@
 
 choco feature enable -n allowGlobalConfirmation
 
-set MY_CHOCO_INSTALL_CMD=choco install -v --yes 
+set MY_CHOCO_INSTALL_CMD=choco install -v --yes
+
+%MY_CHOCO_INSTALL_CMD% choco-upgrade-all-at
 
 %MY_CHOCO_INSTALL_CMD% conemu
 %MY_CHOCO_INSTALL_CMD% curl
 %MY_CHOCO_INSTALL_CMD% firefox
 %MY_CHOCO_INSTALL_CMD% git
-%MY_CHOCO_INSTALL_CMD% gpg4win 
-%MY_CHOCO_INSTALL_CMD% greenshot 
+%MY_CHOCO_INSTALL_CMD% gpg4win
+%MY_CHOCO_INSTALL_CMD% greenshot
 %MY_CHOCO_INSTALL_CMD% keepass
 %MY_CHOCO_INSTALL_CMD% putty
 %MY_CHOCO_INSTALL_CMD% qownnotes
 %MY_CHOCO_INSTALL_CMD% livekd
-%MY_CHOCO_INSTALL_CMD% nextcloud-client 
-%MY_CHOCO_INSTALL_CMD% python2 
-%MY_CHOCO_INSTALL_CMD% spotify
+%MY_CHOCO_INSTALL_CMD% nextcloud-client
+%MY_CHOCO_INSTALL_CMD% python3
 %MY_CHOCO_INSTALL_CMD% sumatrapdf
 %MY_CHOCO_INSTALL_CMD% sysinternals
 %MY_CHOCO_INSTALL_CMD% tccle
@@ -26,12 +27,19 @@ set MY_CHOCO_INSTALL_CMD=choco install -v --yes
 %MY_CHOCO_INSTALL_CMD% tortoisegit
 %MY_CHOCO_INSTALL_CMD% vim
 %MY_CHOCO_INSTALL_CMD% visualstudio2017community
-%MY_CHOCO_INSTALL_CMD% vlc
 %MY_CHOCO_INSTALL_CMD% vscode
-%MY_CHOCO_INSTALL_CMD% vscode-settingssync 
+%MY_CHOCO_INSTALL_CMD% vscode-settingssync
 %MY_CHOCO_INSTALL_CMD% wget
 %MY_CHOCO_INSTALL_CMD% windbg
+%MY_CHOCO_INSTALL_CMD% microsoft-windows-terminal
+
+REM Multimedia stuff.
+%MY_CHOCO_INSTALL_CMD% spotify
+%MY_CHOCO_INSTALL_CMD% vlc
 %MY_CHOCO_INSTALL_CMD% xnviewmp
 
 bcdedit.exe -set loadoptions DDISABLE_INTEGRITY_CHECKS
 bcdedit.exe -set TESTSIGNING ON
+
+REM Disable fast startup (will break dual boot).
+powercfg -h off
